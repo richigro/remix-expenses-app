512a2f22abbb0e427877576c615af2eed36a2e5c
Ricardo Guerrero <richi_gro@hotmail.com>
2023-02-26 15:02:24 -0600

    Add link button for new expenses and to load raw

--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -1,4 +1,6 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, Link } from "@remix-run/react";
+import { FaPlus, FaDownload } from "react-icons/fa";
+
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 export const DUMMY_EXPENSES = [
@@ -21,6 +23,17 @@ const ExpensesLayout = () => {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add expense</span>
+          </Link>
+          {/* this should be an anchor tag because the raw route doesn't have any UI */}
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
