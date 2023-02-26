60948ffc5ebf5dd07f13f920cac53b3403be9a93
Ricardo Guerrero <richi_gro@hotmail.com>
2023-02-26 14:48:47 -0600

    Login form states for signup and login

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,10 +1,20 @@
-import { FaLock } from 'react-icons/fa';
+import { useState } from "react";
+import { Link, useSearchParams } from "@remix-run/react";
+import { FaLock, FaUserPlus } from "react-icons/fa";
 
-function AuthForm() {
+const AuthForm = () => {
+  // const [mode] = useState("login");
+  const [searchParams] = useSearchParams();
+  const authMode = searchParams.get("mode") || "login";
+
+  const submitButtonCaption = authMode === "login" ? "Login" : "Create user";
+  const toggleButtonCaption =
+    authMode === "login" ? "Create a new user" : "Login with existing user";
+
   return (
     <form method="post" className="form" id="auth-form">
       <div className="icon-img">
-        <FaLock />
+        {authMode === "login" ? <FaLock /> : <FaUserPlus />}
       </div>
       <p>
         <label htmlFor="email">Email Address</label>
@@ -15,11 +25,13 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <a href="/auth">Log in with existing user</a>
+        <button>{submitButtonCaption}</button>
+        <Link to={authMode === "login" ? "?mode=signup" : "?mode=login"}>
+          {toggleButtonCaption}
+        </Link>
       </div>
     </form>
   );
-}
+};
 
 export default AuthForm;
